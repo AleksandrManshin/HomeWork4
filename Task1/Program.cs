223e83275ebbff1a4dfcaef59120b0335d099cdb
Aleksandr <maivan007@yandex.ru>
2022-07-07 18:12:20 +0400

    Add Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿void MathPow()
+{
+    Console.Write("Введите число A: ");
+    int A = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число B: ");
+    int B = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Число {A} в степни {B} равно {Math.Pow(A, B)}");
+}
+MathPow();
