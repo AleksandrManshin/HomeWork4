3e607ddd2fda8b7136c9be1395d98fbc192542a4
Aleksandr <maivan007@yandex.ru>
2022-07-07 18:23:23 +0400

    Add Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿void InputAndOutput()
+{
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Сумма цифр числа {number} равна {SumDigits(number)}");
+}
+
+int SumDigits(int number)
+{
+    int result = number % 10;
+    while (number > 0)
+    {
+        number = number / 10;
+        result = result + number % 10;
+    }
+    return result;
+}
+InputAndOutput();
