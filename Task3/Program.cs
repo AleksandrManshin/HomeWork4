d35deffc4fcc05d40e5740d0fc6e62b2b6761d2d
Aleksandr <maivan007@yandex.ru>
2022-07-07 18:27:21 +0400

    Add Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Write("Массив до сортировки: ");
+int[] arr = new int[8];
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(-99, 100);
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (Math.Abs(array[j]) < Math.Abs(array[minPosition])) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+Console.WriteLine();
+Console.Write("Массив после сортировки: ");
+PrintArray(arr);
